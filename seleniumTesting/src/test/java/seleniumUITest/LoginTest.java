cd9e36f1612da34db5f498df132ee04509cd0c73
Adem <Adem@DESKTOP-KONJ4LL.fritz.box>
2023-12-13 21:20:07 +0100

    Getting started

---
+++ seleniumTesting/src/test/java/seleniumUITest/LoginTest.java
@@ -0,0 +1,22 @@
+package seleniumUITest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+		driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+	}
+}
